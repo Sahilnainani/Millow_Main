bb94bdc1537e586ba07ef0ad9d2b403ce53df1ee
Sahil Nainani <sahilnainani45226@gmail.com>
2023-05-04 22:38:50 +0530

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1338
+    }
+  },
+  solidity: "0.8.17",
+};
